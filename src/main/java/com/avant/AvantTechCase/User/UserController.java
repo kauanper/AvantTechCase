78fee70230783ff598bb5593090fb4bf0788800d
kauan <kauancavalcante8p@gmail.com>
2026-05-07 01:46:52 -0300

    feat: pegando userId a partir de user autenticado

--- src/main/java/com/avant/AvantTechCase/User/UserController.java
+++ src/main/java/com/avant/AvantTechCase/User/UserController.java
@@ -5,6 +5,7 @@ import com.avant.AvantTechCase.User.DTOs.UserResponseDTO;
 import com.avant.AvantTechCase.User.services.CreateUserUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +24,17 @@ public class UserController {
         }else{
             return ResponseEntity.ok().body(dto);
         }
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity getPerfil() {
+        UserEntity user = (UserEntity) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        Long id = user.getId();
+        String login = user.getLogin();
+
+        return ResponseEntity.ok("O ID do usuário logado é: " + id);
     }
 }
