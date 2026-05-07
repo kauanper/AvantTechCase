fbd60dec462f6b1f67db77f57b1b525579606ea7
kauan <kauancavalcante8p@gmail.com>
2026-05-06 23:02:22 -0300

    add: entidade user v2

--- src/main/java/com/avant/AvantTechCase/User/User.java
+++ src/main/java/com/avant/AvantTechCase/User/User.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Table
+@Table(name = "users")
 @Entity
 @Data
 @NoArgsConstructor
