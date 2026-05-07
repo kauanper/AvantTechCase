f4b06e5a8f176af5766bb1c2f535f9061d2a6219
kauan <kauancavalcante8p@gmail.com>
2026-05-06 22:16:18 -0300

    first commit

---
+++ src/main/java/com/avant/AvantTechCase/AvantTechCaseApplication.java
@@ -0,0 +1,13 @@
+package com.avant.AvantTechCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvantTechCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvantTechCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avant/AvantTechCase/AvantTechCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avant.AvantTechCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvantTechCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
