868cd0ba32d0a53ee5a7811c48b0df46f1f59889
kauan <kauancavalcante8p@gmail.com>
2026-05-07 00:06:22 -0300

    add: login v3 - passwordEncoder

--- src/main/java/com/avant/AvantTechCase/Configs/Security/SecurityConfigurations.java
+++ src/main/java/com/avant/AvantTechCase/Configs/Security/SecurityConfigurations.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,5 +33,10 @@ public class SecurityConfigurations {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
