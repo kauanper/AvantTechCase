bc67a5d91f47f3535844106e31358a70f6fe54fa
kauan <kauancavalcante8p@gmail.com>
2026-05-06 23:09:05 -0300

    add:  removendo roles de user

--- src/main/java/com/avant/AvantTechCase/User/User.java
+++ src/main/java/com/avant/AvantTechCase/User/User.java
@@ -9,6 +9,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -26,11 +27,10 @@ public class User implements UserDetails {
     private Long id;
     private String login;
     private String password;
-    private Roles role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
