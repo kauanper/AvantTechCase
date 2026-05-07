354d6a460172bea7bdb13635551c3691a6d9a360
kauan <kauancavalcante8p@gmail.com>
2026-05-06 23:20:45 -0300

    add: metodo e serviço de autorização

--- src/main/java/com/avant/AvantTechCase/User/UserRepository.java
+++ src/main/java/com/avant/AvantTechCase/User/UserRepository.java
@@ -1,6 +1,8 @@
 package com.avant.AvantTechCase.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserDetails findByLogin(String login);
 }
